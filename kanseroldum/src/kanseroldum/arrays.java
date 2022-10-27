01c823480fb470fb9d5b90a766dd44fc9b5c187a
aidata <aidata@10.121.3.202>
2022-10-27 11:03:21 +0300

    3

--- kanseroldum/src/kanseroldum/arrays.java
+++ kanseroldum/src/kanseroldum/arrays.java
@@ -5,15 +5,32 @@ public class arrays {
 	public static void main(String[] args) {
 		
 		int[] array = {32,27,64,18,95,14,90,70,60,37};
+		int gecici;
 	
 		System.out.printf("%s%8s%n","Index","Value");
 		
 		for(int counter =0; counter<array.length;counter++) {
 			
 			System.out.printf("%5d %8d d%n",counter,array[counter]);
+			
+			
 		}
 			
-
+		System.out.println("Dizinin Tersi: ");
+		
+		
+		
+		System.out.println("Dizinin Tersi: ");
+		
+		for(int i =array.length - 1; i>=0;i--) {
+			
+			
+			System.out.println(array[i]+" ");
+			
+		}
+		
+		
+		
 	}
 
 }
